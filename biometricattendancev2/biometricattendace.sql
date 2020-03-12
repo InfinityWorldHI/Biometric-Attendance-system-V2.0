67db6a5580bccdc29ea27357c618ba18aa517ea0
Electronics Tech HaIs <41703435+InfinityWorldHI@users.noreply.github.com>
2020-03-12 16:14:11 +0200

    Add files via upload

---
+++ biometricattendancev2/biometricattendace.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 11, 2020 at 08:20 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `biometricattendace`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `admin_name` varchar(30) NOT NULL,
+  `admin_email` varchar(80) NOT NULL,
+  `admin_pwd` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `admin_name`, `admin_email`, `admin_pwd`) VALUES
+(1, 'Admin', 'admin@gmail.com', '$2y$10$89uX3LBy4mlU/DcBveQ1l.32nSianDP/E1MfUh.Z.6B4Z0ql3y7PK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `devices`
+--
+
+CREATE TABLE `devices` (
+  `id` int(11) NOT NULL,
+  `device_name` varchar(50) NOT NULL,
+  `device_dep` varchar(20) NOT NULL,
+  `device_uid` text NOT NULL,
+  `device_date` date NOT NULL,
+  `device_mode` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pwd_reset`
+--
+
+CREATE TABLE `pwd_reset` (
+  `pwd_reset_id` int(11) NOT NULL,
+  `pwd_reset_email` varchar(50) NOT NULL,
+  `pwd_reset_selector` text NOT NULL,
+  `pwd_reset_token` longtext NOT NULL,
+  `pwd_reset_expires` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL DEFAULT 'None',
+  `serialnumber` double NOT NULL DEFAULT '0',
+  `gender` varchar(10) NOT NULL DEFAULT 'None',
+  `email` varchar(50) NOT NULL DEFAULT 'None',
+  `fingerprint_id` int(11) NOT NULL,
+  `fingerprint_select` tinyint(1) NOT NULL DEFAULT '0',
+  `user_date` date NOT NULL,
+  `device_uid` varchar(20) NOT NULL DEFAULT '0',
+  `device_dep` varchar(20) NOT NULL DEFAULT '0',
+  `del_fingerid` tinyint(1) NOT NULL DEFAULT '0',
+  `add_fingerid` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_logs`
+--
+
+CREATE TABLE `users_logs` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `serialnumber` double NOT NULL,
+  `fingerprint_id` int(5) NOT NULL,
+  `device_uid` varchar(20) NOT NULL,
+  `device_dep` varchar(20) NOT NULL,
+  `checkindate` date NOT NULL,
+  `timein` time NOT NULL,
+  `timeout` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `devices`
+--
+ALTER TABLE `devices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pwd_reset`
+--
+ALTER TABLE `pwd_reset`
+  ADD PRIMARY KEY (`pwd_reset_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_logs`
+--
+ALTER TABLE `users_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `devices`
+--
+ALTER TABLE `devices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pwd_reset`
+--
+ALTER TABLE `pwd_reset`
+  MODIFY `pwd_reset_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users_logs`
+--
+ALTER TABLE `users_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
